3af61b3a33caac15a174ee91c6ccd7b5cf4b6a52
Holik55 <202110715057@mhs.ubharajaya.ac.id>
2025-06-16 19:59:47 +0700

    3

--- backend/app.js
+++ backend/app.js
@@ -9,6 +9,7 @@ const booksRoutes = require('./routes/books');
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
+app.use(express.json());
 
 app.use('/api/auth', authRoutes);
 app.use('/api/books', booksRoutes);
