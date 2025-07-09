65cf7f5c4b3bdcab6ea86e8ad633266c39ba6254
Holik55 <202110715057@mhs.ubharajaya.ac.id>
2025-07-09 21:39:34 +0700

    coba hosting

--- backend/app.js
+++ backend/app.js
@@ -15,7 +15,7 @@ app.use('/api/auth', authRoutes);
 app.use('/api/books', booksRoutes);
 app.use('/api', loanRoutes);
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
